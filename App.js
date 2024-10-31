abaf9ebc827a583201821f87e2bd4bf1e6a515ee
Syariful Amar <kingnazaer221@gmail.com>
2024-10-31 22:00:57 +0700

    menerjemahkan ekspresi alert dari bahasa inggris kedalam bahasa indoonesia

--- App.js
+++ App.js
@@ -37,7 +37,7 @@ const App = () => {
       setHasil(eval(hasil).toString());
       setLastChar('');
     } catch (error) {
-      Alert.alert('Error in expression');
+      Alert.alert('Perhitungan Salah');
     }
   };
 
